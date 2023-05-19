6c579c8a181f1fe3f7126a71f56d4a012d72056e
wallace-y <cameronwfinney@gmail.com>
2023-05-19 14:09:43 +0100

    Adding sort to the default stock list

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -9,10 +9,18 @@ function Container() {
     { name: "bread", quantity: 50 },
     { name: "eggs", quantity: 50 },
   ];
-  const [StockListOfItems, setStockListOfItems] = useState(initialStockList);
+
+  const sortedStock = initialStockList.sort((a, b) => {
+    let textA = a.name.toUpperCase();
+    let textB = b.name.toUpperCase();
+    return textA < textB ? -1 : textA > textB ? 1 : 0;
+  });
+
+  const [StockListOfItems, setStockListOfItems] = useState(sortedStock);
 
   const [shoppingListOfItems, setShoppingList] = useState([]);
 
+  initialStockList;
   return (
     <div className="d-flex flex-row justify-content-between mb-3">
       <StockList
